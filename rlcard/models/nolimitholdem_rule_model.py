5b4f90f6a22d565f02673568489a7658c812ee9f
Jens Verelst <jens.verelst@student.uantwerpen.be>
2024-04-15 19:38:33 +0200

    added all rule based models

--- rlcard/models/nolimitholdem_rule_model.py
+++ rlcard/models/nolimitholdem_rule_model.py
@@ -1,13 +1,5 @@
 import rlcard
 from rlcard.models.model import Model
-from enum import Enum
-
-class Action(Enum):
-    FOLD = 0
-    CHECK_CALL = 1
-    RAISE_HALF_POT = 2
-    RAISE_POT = 3
-    ALL_IN = 4
 
 class UnlimitedHoldemRuleAgentV1(object):
     ''' Unlimited Texas Hold'em Rule agent version 1
@@ -23,27 +15,27 @@ class UnlimitedHoldemRuleAgentV1(object):
             state (dict): Raw state from the game
 
         Returns:
-            action (str): Predicted action
+            action (int): Predicted action as a numerical value
         '''
         legal_actions = state['raw_legal_actions']
         state = state['raw_obs']
         hand = state['hand']
         public_cards = state['public_cards']
-        action = Action.FOLD.name  # Default action is FOLD
+        action = 0  # Default action is FOLD
 
         # Calculate the strength of the hand
         hand_strength = calculate_hand_strength(hand, public_cards)
 
         # Decide actions based on the strength of the hand and current betting round
-        if Action.FOLD.name in legal_actions:
+        if 0 in legal_actions:
             if hand_strength == 'HIGH_PAIR' or hand_strength == 'TWO_PAIR' or hand_strength == 'THREE_OF_A_KIND':
-                action = Action.RAISE_POT.name  # Raise with strong hands
+                action = 3  # Raise with strong hands
             elif hand_strength == 'STRAIGHT' or hand_strength == 'FLUSH' or hand_strength == 'FULL_HOUSE' or hand_strength == 'FOUR_OF_A_KIND':
-                action = Action.ALL_IN.name  # Go all-in with very strong hands
-            elif Action.RAISE_POT.name in legal_actions:
-                action = Action.RAISE_POT.name  # Raise if possible
+                action = 4  # Go all-in with very strong hands
+            elif 3 in legal_actions:
+                action = 3  # Raise if possible
             else:
-                action = Action.CHECK_CALL.name  # Otherwise, check or call
+                action = 1  # Otherwise, check or call
 
         return action
 
