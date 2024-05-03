a29c2faee7fa90a059cef6d3f5a96c135cf87f7f
Jens Verelst <jens.verelst@student.uantwerpen.be>
2024-05-03 16:54:16 +0200

    classification table

--- rlcard/utils/logger.py
+++ rlcard/utils/logger.py
@@ -16,16 +16,28 @@ class Logger(object):
     def __enter__(self):
         self.txt_path = os.path.join(self.log_dir, 'log.txt')
         self.csv_path = os.path.join(self.log_dir, 'performance.csv')
+        self.csv_reward_path = os.path.join(self.log_dir, 'reward.csv')
+        self.csv_loss_path = os.path.join(self.log_dir, 'loss.csv')
         self.fig_path = os.path.join(self.log_dir, 'fig.png')
+        self.rewardfig_path = os.path.join(self.log_dir, 'rewardfig.png')
+        self.lossfig_path = os.path.join(self.log_dir, 'lossfig.png')
 
         if not os.path.exists(self.log_dir):
             os.makedirs(self.log_dir)
 
         self.txt_file = open(self.txt_path, 'w')
         self.csv_file = open(self.csv_path, 'w')
+        self.csv_reward_file = open(self.csv_reward_path, 'w')
+        self.csv_loss_file = open(self.csv_loss_path, 'w')
         fieldnames = ['episode', 'loss']
+        rewardfieldnames = ['episode','reward']
+        lossfieldnames = ['episode','loss']
         self.writer = csv.DictWriter(self.csv_file, fieldnames=fieldnames)
+        self.rewardWriter = csv.DictWriter(self.csv_reward_file, fieldnames=rewardfieldnames)
+        self.lossWriter = csv.DictWriter(self.csv_loss_file, fieldnames=lossfieldnames)
         self.writer.writeheader()
+        self.rewardWriter.writeheader()
+        self.lossWriter.writeheader()
 
         return self
 
@@ -50,10 +62,38 @@ class Logger(object):
         self.log('  episode      |  ' + str(episode))
         self.log('  loss       |  ' + str(reward))
         self.log('----------------------------------------')
+    def log_loss(self, episode, loss):
+        ''' Log a point in the curve
+        Args:
+            episode (int): the episode of the current point
+            loss (float): the reward of the current point
+        '''
+        self.lossWriter.writerow({'episode': episode, 'loss': loss})
+        print('')
+        self.log('----------------------------------------')
+        self.log('  episode      |  ' + str(episode))
+        self.log('  loss       |  ' + str(loss))
+        self.log('----------------------------------------')
 
+    def log_reward(self, episode, reward):
+        ''' Log a point in the curve
+        Args:
+            episode (int): the episode of the current point
+            reward (float): the reward of the current point
+        '''
+        self.rewardWriter.writerow({'episode': episode, 'loss': reward})
+        print('')
+        self.log('----------------------------------------')
+        self.log('  episode      |  ' + str(episode))
+        self.log('  loss       |  ' + str(reward))
+        self.log('----------------------------------------')
     def __exit__(self, type, value, traceback):
         if self.txt_path is not None:
             self.txt_file.close()
         if self.csv_path is not None:
             self.csv_file.close()
+        if self.csv_reward_path is not None:
+            self.csv_reward_file.close()
+        if self.csv_loss_path is not None:
+            self.csv_loss_file.close()
         print('\nLogs saved in', self.log_dir)
