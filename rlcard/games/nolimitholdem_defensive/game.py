8afe3bcf5878023805e8c74c850726b0b5cb5a89
Jens Verelst <jens.verelst@student.uantwerpen.be>
2024-06-09 17:26:11 +0200

    aggressive 5

--- rlcard/games/nolimitholdem_defensive/game.py
+++ rlcard/games/nolimitholdem_defensive/game.py
@@ -291,7 +291,7 @@ class NolimitholdemGame(Game):
         payoffs = np.array(chips_payoffs) / self.big_blind
         for i in range(len(payoffs)):
             if payoffs[i] < 0.00:
-                payoffs[i] = payoffs[i] * 2
+                payoffs[i] = payoffs[i] * 5
         return payoffs
 
     def safe_reward(self):
